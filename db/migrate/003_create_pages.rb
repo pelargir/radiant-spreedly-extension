a251680d94f438e73a78cae5ffc2af8a6727079b
Matthew Bass <matt@anacreon.local>
2008-11-13 10:35:48 -0500

    added migration to generate sample pages and page parts

--- db/migrate/003_create_pages.rb
+++ db/migrate/003_create_pages.rb
@@ -1,10 +1,63 @@
 class CreatePages < ActiveRecord::Migration
   def self.up
-    #<SubscribersOnlyPage id: 10, title: "Subscriber", slug: "subscriber", breadcrumb: "Subscriber", class_name: "SubscribersOnlyPage", status_id: 100, parent_id: 1, layout_id: nil, created_at: "2008-11-12 14:39:39", updated_at: "2008-11-12 17:55:54", published_at: "2008-11-12 09:39:39", created_by_id: 1, updated_by_id: 1, virtual: false, lock_version: 10, description: "", keywords: "">
-    #<Page id: 11, title: "Login", slug: "login", breadcrumb: "Login", class_name: "Page", status_id: 100, parent_id: 10, layout_id: nil, created_at: "2008-11-12 14:40:13", updated_at: "2008-11-12 14:57:14", published_at: "2008-11-12 09:40:13", created_by_id: 1, updated_by_id: 1, virtual: false, lock_version: 3, description: "", keywords: "">
-    #<Page id: 12, title: "Register", slug: "register", breadcrumb: "Register", class_name: "Page", status_id: 100, parent_id: 10, layout_id: nil, created_at: "2008-11-12 17:56:24", updated_at: "2008-11-12 18:11:35", published_at: "2008-11-12 12:56:24", created_by_id: 1, updated_by_id: 1, virtual: false, lock_version: 4, description: "", keywords: "">
+    home_page = Page.find(:first, :conditions => {:parent_id => nil})
+    
+    subscriber_page = SubscribersOnlyPage.new do |p|
+      p.title = "Subscriber"
+      p.slug = "subscriber"
+      p.breadcrumb = "Subscriber"
+      p.class_name = "SubscribersOnlyPage"
+      p.status_id = 100
+      p.parent = home_page
+      p.published_at = Time.now
+    end
+    subscriber_page.save!
+    
+    PagePart.new do |p|
+      p.name = "body"
+      p.filter_id = "Textile"
+      p.content = "<r:flash:notices />\r\n\r\n*Username:* <r:subscriber:username />\r\n\r\n*Subscription:* <r:subscriber:subscription />\r\n\r\n<r:subscriber:refresh />\r\n\r\n<r:subscriber:logout />\r\n"
+      p.page = subscriber_page
+    end.save!
+    
+    login_page = Page.new do |p|
+      p.title = "Login"
+      p.slug = "login"
+      p.breadcrumb = "Login"
+      p.class_name = "Page"
+      p.status_id = 100
+      p.parent = subscriber_page
+      p.published_at = Time.now
+    end
+    login_page.save!
+    
+    PagePart.new do |p|
+      p.name = "body"
+      p.filter_id = "Textile"
+      p.content = "<r:flash:notices />\r\n\r\n<r:subscriber:login:form />\r\n\r\n<r:subscriber:register:link />"
+      p.page = login_page
+    end.save!
+    
+    register_page = Page.new do |p|
+      p.title = "Register"
+      p.slug = "register"
+      p.breadcrumb = "Register"
+      p.class_name = "Page"
+      p.status_id = 100
+      p.parent = subscriber_page
+      p.published_at = Time.now
+    end
+    register_page.save!
+    
+    PagePart.new do |p|
+      p.name = "body"
+      p.filter_id = "Textile"
+      p.content = "<r:flash:notices />\r\n\r\n<r:subscriber:register:form />\r\n\r\n\"Go back\":/subscriber/login"
+      p.page = register_page
+    end.save!
   end
 
   def self.down
+    SubscribersOnlyPage.find_by_title("Subscriber").destroy
   end
 end
